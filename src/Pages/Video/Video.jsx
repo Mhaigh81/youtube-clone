9c00ece184880829c46deef6363423aa0893b3f4
Mhaigh81 <mhaigh@me.com>
2026-02-05 13:32:23 -0500

    gettting video id undefined

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommended from '../../Components/Recommended/Recommended.jsx'
+import { useParams } from 'react-router-dom'
+
 
 const Video = () => {
+
+  const { videoId, categoryId } = useParams()
+
   return (
     <div className='play-container'>
-      <PlayVideo />
+      {/* <PlayVideo /> */}
+      <PlayVideo videoId={videoId} />
+      <Recommended />
     </div>
   )
 }
